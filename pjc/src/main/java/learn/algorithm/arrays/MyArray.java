46835247c75ec160ffaa89464865fcae32ce074a
micheal <996770483@qq.com>
2020-06-24 13:18:20 +0800

    数组

---
+++ pjc/src/main/java/learn/algorithm/arrays/MyArray.java
@@ -0,0 +1,31 @@
+package learn.algorithm.arrays;
+
+/**
+ * 自定义 数组
+ */
+public class MyArray<E> {
+    private E[] data;
+    private int size;
+
+    public MyArray(int capacity) {
+//        data = new E[capacity]; 不支持这样做
+        data = (E[])new Object[capacity];
+        size = 0;
+    }
+
+    public MyArray(){
+        this(10);
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getCapacity(){
+        return data.length;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+}
---
+++ pjc/src/main/java/learn/algorithm/arrays/Run.java
@@ -0,0 +1,19 @@
+package learn.algorithm.arrays;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 使用
+ */
+public class Run {
+
+    @Test
+    public void testMyArray(){
+        List<Integer> integerList = new ArrayList<>();
+        integerList.remove(2);
+    }
+
+}
