874b62e767362b17475cdcc29a04504f9a27cb0f
micheal <996770483@qq.com>
2020-06-30 13:49:56 +0800

    映射

---
+++ pjc/src/main/java/learn/algorithm/playstruct/setAndMap/Run.java
@@ -0,0 +1,47 @@
+package learn.algorithm.playstruct.setAndMap;
+
+import learn.algorithm.playstruct.setAndMap.demo.FileOperation;
+
+import java.util.ArrayList;
+
+/**
+ * 测试 时间复杂度
+ */
+public class Run {
+
+
+    private static double testSet(Set<String> set, String filename){
+
+        long startTime = System.nanoTime();
+
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if(FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words)
+                set.add(word);
+            System.out.println("Total different words: " + set.getSize());
+        }
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        String filename = "D:\\code\\texts\\pride-and-prejudice.txt";
+
+        BSTSet<String> bstSet = new BSTSet<>();
+        double time1 = testSet(bstSet, filename);
+        System.out.println("BST Set: " + time1 + " s");
+
+        System.out.println();
+
+        LinkedListSet<String> linkedListSet = new LinkedListSet<>();
+        double time2 = testSet(linkedListSet, filename);
+        System.out.println("Linked List Set: " + time2 + " s");
+
+    }
+
+}
